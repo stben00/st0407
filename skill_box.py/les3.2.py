961ebc3b4787ce1f26028a05d70618e81199ecde
stben00 <bekzatruslanuulu@gmail.com>
2025-09-13 20:14:08 +0000

    Сохранил файл

---
+++ skill_box.py/les3.2.py
@@ -0,0 +1,74 @@
+# print(6 * 39)
+# print(3 ** 5)
+# print(-8 / -4)
+# print(10 / 2 + 6)
+
+
+
+# a, b, c = 5, 4, 2
+
+# print("addition: ", a + b + c)
+
+# print("subtraction: ",  a - b - c)
+
+# print("Multiplication: ", a * b * c)
+
+# print("Division: ", a / b / c)
+
+# print("Power: ", a ** b ** c)
+
+
+# print(65 / 0)
+
+
+
+                                        #### 3.3 ####
+
+
+# a, b, c = 8, 5, 3
+
+# result = ((a /(b + c)) - a) / c 
+# print("Result: ", result)
+
+
+
+# bus = 5
+# metro = 3
+
+# result = ( 6+ (10 - bus) ** 2) / (metro + 1) + 132 /(2 + bus)
+# print(result)
+
+
+# numerator = -3 +( 8 ** 2 - 12) * (4 ** (3 ** 2))
+# denomirator = 2 * 18
+
+# result = numerator / denomirator
+# print(result)
+                                  #### 3.4 ####
+
+
+
+# x = int(input("Введите первое число: "))
+# y = int(input("Введите второе число: "))
+
+# result = x + y
+
+# print("Сумма : ", result)
+
+
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# c = int(input("Введите третье число: "))
+# d = int(input("Введите четвертое число: "))
+
+# result = 2 * (c + 5 + (a * b) / (4 * b)) * (d - (2 * (a ** 3)) / 30) - 10
+# print("Ответ: ", result)
+
+
+
+# a = '2'
+# b = '5'
+# c = '3'
+# num = 6 ** int(a) + (7 - int(b)) * int(c)
+# print(num)пш
