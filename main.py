5c69ffd7a43338f92413208cfdc368ffd4b64c32
stben00 <bekzatruslanuulu@gmail.com>
2025-09-17 19:51:44 +0000

    Обновил код

--- main.py
+++ main.py
@@ -51,3 +51,70 @@ def calculator():
 calculator()
  
 
+
+
+
+
+
+ 
+
+import random
+
+print("Добро пожаловать в игру 'Разминка для Мозга ' !")
+print("Твоя задача сладывать правильно числа. Поехали!\n ")
+
+score = 0
+
+for i in range(5):
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    
+    print(f"Пример {i + 1}: {a} + {b} = ?")
+    user_answer = int(input("Ваш ответ: "))
+    
+    
+    correct_answer = a + b
+    
+    if user_answer == correct_answer:
+        print("Правильный ответ !\n")
+        score += 1
+    
+    else: 
+        print("Ответ неверный !")
+        print("Правильный результат: ", correct_answer, "\n")
+print(f"Игра окончена!. Ты набрал {score} из 5 баллов. ")
+    
+    
+    
+
+secret_password = 123
+max_attempts = 3
+attempts = 0
+success  = False
+
+while attempts < max_attempts:
+    try:
+        password = int(input("Enter your password: "))
+        
+        if password == secret_password:
+            print("Welcome")
+            success = True
+            break
+        else:
+          attempts += 1
+          print(f"Error: Invalid password. Attempts left : {max_attempts - attempts}")
+            
+    except ValueError:
+        attempts += 1
+        print(f"Use only numbers. Attempts left: {max_attempts - attempts}")
+        
+if not success:
+    print("You habe used all attempts. Access denied")
+
+
+
+    
+
+    
+    
+    
